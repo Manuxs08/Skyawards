cbc7cf7ca9e6d8f07776aae5ff5dae42b35bce51
Manuxs08 <manuelarotinco@gmail.com>
2025-01-15 11:12:05 -0500

    Change server v4

--- server/database/index.js
+++ server/database/index.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    connectionLimit: 10,
+    connectionLimit: 100,
     waitForConnections: true,
     queueLimit: 0
 })
