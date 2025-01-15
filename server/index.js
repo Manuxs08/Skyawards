77faebe21b6d9cfc888df1698ad8f1f478e18385
Manuxs08 <manuelarotinco@gmail.com>
2025-01-15 18:49:47 -0500

    Change server v11

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3306
+const PORT = process.env.MYSQLPORT || 3306
 
 const db = require('./config')
 
