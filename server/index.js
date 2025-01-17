4dd5dd2ae209d35e3168ee700abb4e64a5d2af70
Manuxs08 <manuelarotinco@gmail.com>
2025-01-16 19:11:46 -0500

    Change server v22

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://skyawards-client.vercel.app',
+    origin: ['https://skyawards-client.vercel.app','https://skyawards-client.vercel.app/categoria'],
     credentials: true
 }
 ));
