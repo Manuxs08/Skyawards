0d3c393a0cf8bbd70e47fa70c3dda02e4f2bb142
Manuxs08 <manuelarotinco@gmail.com>
2025-01-17 12:35:33 -0500

    Change server v33

--- server/index.js
+++ server/index.js
@@ -40,7 +40,6 @@ app.get('/get/categorias', async (req, res) => {
             "SELECT * FROM categorias"
         );
         if(results.length > 0){
-            console.log(results)
             return res.status(200).json(results)
         }else{
             res.status(401).send("Consulta fallida")
@@ -50,101 +49,171 @@ app.get('/get/categorias', async (req, res) => {
     }
 })
 
-app.get('/get/categoria/nominados', (req, res) => {
-    const sql = "SELECT * FROM `nominadoscategoria`";
-    db.query(sql, (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+app.get('/get/categoria/nominados', async (req, res) => {
+    try {
+        const [results, fields] = await db.query(
+            "SELECT * FROM `nominadoscategoria`"
+        );
+        if(results.length > 0){
+            return res.status(200).json(results)
+        }else{
+            res.status(401).send("Consulta fallida")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.get('/get/categorias/nominados/idCat', (req, res) => {
-    const sql = "SELECT * FROM `nominadoscategoria` WHERE idCategoria = ?";
+app.get('/get/categorias/nominados/idCat', async (req, res) => {
     const idCategoria = req.query.idCategoria;
-    db.query(sql, idCategoria, (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        const [results, fields] = await db.query(
+            "SELECT * FROM `nominadoscategoria` WHERE idCategoria = ?",
+            [idCategoria]
+        );
+        if(results.length > 0){
+            return res.status(200).json(results)
+        }else{
+            res.status(401).send("Consulta fallida")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.get('/get/categoria/nominados/count', (req, res) => {
-    const sql = "SELECT COUNT(*) as votes FROM `votos` WHERE `idNomCategoria` = ?";
+app.get('/get/categoria/nominados/count', async (req, res) => {
     const id = req.query.id;
-    db.query(sql, id, (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        const [results, fields] = await db.query(
+            "SELECT COUNT(*) as votes FROM `votos` WHERE `idNomCategoria` = ?",
+            [id]
+        );
+        if(results.length > 0){
+            return res.status(200).json(results)
+        }else{
+            res.status(401).send("Consulta fallida")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.get('/get/usuario/:idVotante', (req, res) => {
-    const sql = "SELECT * FROM `usuarios` WHERE `id` = ?";
+app.get('/get/usuario/:idVotante', async (req, res) => {
     const id = req.params.idVotante
-    db.query(sql, id, (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        const [results, fields] = await db.query(
+            "SELECT * FROM `usuarios` WHERE `id` = ?",
+            [id]
+        );
+        if(results.length > 0){
+            return res.status(200).json(results)
+        }else{
+            res.status(401).send("Consulta fallida")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.post('/post/usuario', (req, res) => {
-    const sql = "INSERT INTO `usuarios`(`id`, `email`, `icon`) VALUES (?)";
-    const values = [
+app.post('/post/usuario', async (req, res) => {
+    const data = [
         req.body.id,
         req.body.email,
         req.body.icon
     ]
-    db.query(sql, [values], (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        await db.query(
+            "INSERT INTO `usuarios`(`id`, `email`, `icon`) VALUES (?)",
+            [data],
+            (err, result) => {
+                if(err){
+                    return res.status(500).json({message: "Consulta fallida"})
+                }
+                res.status(201).json({message: "Datos insertados exitosamente"})
+            });
+    } catch (error) {   
+        console.log(error);
+    }
 })
 
-app.get('/get/votos', (req, res) => {
-    const sql = "SELECT * FROM `votos` WHERE `idVotante` = ?";
+app.get('/get/votos', async (req, res) => {
     const id = req.query.id;
-    db.query(sql, id, (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        const [results, fields] = await db.query(
+            "SELECT * FROM `votos` WHERE `idVotante` = ?",
+            [id]
+        );
+        if(results.length > 0){
+            return res.status(200).json(results)
+        }else{
+            res.status(401).send("Consulta fallida")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.get('/get/voto', (req, res) => {
-    const sql = "SELECT * FROM `votos` WHERE `idVotante` = ? AND `idCategoria` = ?";
-    const idVotante = req.query.idVotante;
-    const idCategoria = req.query.idCategoria;
-    db.query(sql, [idVotante, idCategoria], (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+app.get('/get/voto', async (req, res) => {
+    const data = req.query;
+    try {
+        const [results, fields] = await db.query(
+            "SELECT * FROM `votos` WHERE `idVotante` = ? AND `idCategoria` = ?",
+            [data.idVotante, data.idCategoria]
+        );
+        if(results.length > 0){
+            return res.status(200).json(results)
+        }else{
+            res.status(401).send("Consulta fallida")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.post('/post/voto', (req, res) => {
-    const sql = "INSERT INTO `votos`(`idVotante`, `idCategoria`, `idNominado1`, `idNominado2`, `idNomCategoria`) VALUES (?)";
-    const values = [
+app.post('/post/voto', async (req, res) => {
+    const data = [
         req.body.idVotante,
         req.body.idCategoria,
         req.body.idNominado1,
         req.body.idNominado2,
         req.body.idNomCategoria
     ]
-    db.query(sql, [values], (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        await db.query(
+            "INSERT INTO `votos`(`idVotante`, `idCategoria`, `idNominado1`, `idNominado2`, `idNomCategoria`) VALUES (?)",
+            [data],
+            (err, result) => {
+                if(err){
+                    return res.status(500).json({message: "Consulta fallida"})
+                }
+                res.status(201).json({message: "Datos insertados exitosamente"})
+            });
+    } catch (error) {   
+        console.log(error);
+    }
 })
 
-app.put('/put/voto', (req, res) => {
-    const sql = "UPDATE `votos` SET `idNominado1`= ?,`idNominado2`= ?, `idNomCategoria`= ? WHERE `idVotante` = ? AND `idCategoria` = ?";
-    const values = [
+app.put('/put/voto', async (req, res) => {
+    const data = [
         req.body.idNominado1,
         req.body.idNominado2,
         req.body.idNomCategoria,
         req.body.idVotante,
         req.body.idCategoria
     ]
-    db.query(sql, values, (err, result) => {
-        if(err){return res.json(err)};
-        return res.json(result);
-    })
+    try {
+        await db.query(
+            "UPDATE `votos` SET `idNominado1`= ?,`idNominado2`= ?, `idNomCategoria`= ? WHERE `idVotante` = ? AND `idCategoria` = ?",
+            data,
+            (err, result) => {
+                if(err){
+                    return res.status(500).json({message: "Consulta fallida"})
+                }
+                res.status(200).json({message: "Datos insertados exitosamente"})
+            });
+    } catch (error) {   
+        console.log(error);
+    }
 })
 
 
