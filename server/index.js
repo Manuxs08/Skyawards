e979e81c99fe1c2ec697b8b49a0fc711f3eb4a67
Manuxs08 <manuelarotinco@gmail.com>
2025-01-19 11:52:49 -0500

    Change server v35

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://skyawards-client.vercel.app',
+    origin: ['https://skyawards-client.vercel.app','http://localhost:5173'],
     credentials: true
 }
 ));
